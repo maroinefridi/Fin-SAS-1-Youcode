f6e8fdfa52f333c5c74a4077910af3ecfdb3f166
maroinefridi <maroinehunter@gmail.com>
2026-09-09 17:54:09 +0200

    creerticket

--- Railway Manager/Railway Manager.js
+++ Railway Manager/Railway Manager.js
@@ -2,188 +2,188 @@
 
 prompt = require(`prompt-sync`)()
 
-        const trips = [
-            {
-                id: 1,
-                departure: "Safi",
-                destination: "Youssoufia",
-                departureTime: "07:30",
-                arrivalTime: "08:30",
-                price: 25,
-                availableSeats: 50
-            },
-            {
-                id: 2,
-                departure: "Safi",
-                destination: "Marrakech",
-                departureTime: "08:00",
-                arrivalTime: "10:30",
-                price: 90,
-                availableSeats: 50
-            },
-            {
-                id: 3,
-                departure: "Safi",
-                destination: "Casablanca",
-                departureTime: "09:00",
-                arrivalTime: "13:00",
-                price: 140,
-                availableSeats: 50
-            },
-            {
-                id: 4,
-                departure: "Youssoufia",
-                destination: "Marrakech",
-                departureTime: "09:15",
-                arrivalTime: "11:00",
-                price: 65,
-                availableSeats: 50
-            },
-            {
-                id: 5,
-                departure: "Youssoufia",
-                destination: "Casablanca",
-                departureTime: "10:00",
-                arrivalTime: "13:30",
-                price: 110,
-                availableSeats: 50
-            },
-            {
-                id: 6,
-                departure: "Marrakech",
-                destination: "Casablanca",
-                departureTime: "11:30",
-                arrivalTime: "14:30",
-                price: 120,
-                availableSeats: 50
-            },
-            {
-                id: 7,
-                departure: "Marrakech",
-                destination: "Rabat",
-                departureTime: "12:00",
-                arrivalTime: "16:00",
-                price: 150,
-                availableSeats: 50
-            },
-            {
-                id: 8,
-                departure: "Casablanca",
-                destination: "Rabat",
-                departureTime: "14:00",
-                arrivalTime: "15:15",
-                price: 40,
-                availableSeats: 50
-            },
-            {
-                id: 9,
-                departure: "Casablanca",
-                destination: "Kenitra",
-                departureTime: "15:00",
-                arrivalTime: "16:45",
-                price: 55,
-                availableSeats: 50
-            },
-            {
-                id: 10,
-                departure: "Rabat",
-                destination: "Kenitra",
-                departureTime: "16:00",
-                arrivalTime: "16:45",
-                price: 30,
-                availableSeats: 50
-            },
-            {
-                id: 11,
-                departure: "Rabat",
-                destination: "Fes",
-                departureTime: "17:00",
-                arrivalTime: "19:30",
-                price: 95,
-                availableSeats: 50
-            },
-            {
-                id: 12,
-                departure: "Kenitra",
-                destination: "Fes",
-                departureTime: "17:30",
-                arrivalTime: "20:00",
-                price: 85,
-                availableSeats: 50
-            },
-            {
-                id: 13,
-                departure: "Fes",
-                destination: "Meknes",
-                departureTime: "08:30",
-                arrivalTime: "09:20",
-                price: 35,
-                availableSeats: 50
-            },
-            {
-                id: 14,
-                departure: "Fes",
-                destination: "Oujda",
-                departureTime: "10:00",
-                arrivalTime: "13:30",
-                price: 130,
-                availableSeats: 50
-            },
-            {
-                id: 15,
-                departure: "Meknes",
-                destination: "Rabat",
-                departureTime: "11:00",
-                arrivalTime: "13:30",
-                price: 80,
-                availableSeats: 50
-            },
-            {
-                id: 16,
-                departure: "Meknes",
-                destination: "Casablanca",
-                departureTime: "12:00",
-                arrivalTime: "15:00",
-                price: 105,
-                availableSeats: 50
-            },
-            {
-                id: 17,
-                departure: "Casablanca",
-                destination: "El Jadida",
-                departureTime: "16:30",
-                arrivalTime: "18:00",
-                price: 50,
-                availableSeats: 50
-            },
-            {
-                id: 18,
-                departure: "El Jadida",
-                destination: "Safi",
-                departureTime: "18:30",
-                arrivalTime: "20:30",
-                price: 60,
-                availableSeats: 50
-            },
-            {
-                id: 19,
-                departure: "Marrakech",
-                destination: "Agadir",
-                departureTime: "15:00",
-                arrivalTime: "18:30",
-                price: 100,
-                availableSeats: 50
-            },
-            {
-                id: 20,
-                departure: "Agadir",
-                destination: "Safi",
-                departureTime: "19:00",
-                arrivalTime: "22:00",
-                price: 95,
-                availableSeats: 50
-            }
-        ];
+const trips = [
+    {
+        id: 1,
+        departure: "Safi",
+        destination: "Youssoufia",
+        departureTime: "07:30",
+        arrivalTime: "08:30",
+        price: 25,
+        availableSeats: 50
+    },
+    {
+        id: 2,
+        departure: "Safi",
+        destination: "Marrakech",
+        departureTime: "08:00",
+        arrivalTime: "10:30",
+        price: 90,
+        availableSeats: 50
+    },
+    {
+        id: 3,
+        departure: "Safi",
+        destination: "Casablanca",
+        departureTime: "09:00",
+        arrivalTime: "13:00",
+        price: 140,
+        availableSeats: 50
+    },
+    {
+        id: 4,
+        departure: "Youssoufia",
+        destination: "Marrakech",
+        departureTime: "09:15",
+        arrivalTime: "11:00",
+        price: 65,
+        availableSeats: 50
+    },
+    {
+        id: 5,
+        departure: "Youssoufia",
+        destination: "Casablanca",
+        departureTime: "10:00",
+        arrivalTime: "13:30",
+        price: 110,
+        availableSeats: 50
+    },
+    {
+        id: 6,
+        departure: "Marrakech",
+        destination: "Casablanca",
+        departureTime: "11:30",
+        arrivalTime: "14:30",
+        price: 120,
+        availableSeats: 50
+    },
+    {
+        id: 7,
+        departure: "Marrakech",
+        destination: "Rabat",
+        departureTime: "12:00",
+        arrivalTime: "16:00",
+        price: 150,
+        availableSeats: 50
+    },
+    {
+        id: 8,
+        departure: "Casablanca",
+        destination: "Rabat",
+        departureTime: "14:00",
+        arrivalTime: "15:15",
+        price: 40,
+        availableSeats: 50
+    },
+    {
+        id: 9,
+        departure: "Casablanca",
+        destination: "Kenitra",
+        departureTime: "15:00",
+        arrivalTime: "16:45",
+        price: 55,
+        availableSeats: 50
+    },
+    {
+        id: 10,
+        departure: "Rabat",
+        destination: "Kenitra",
+        departureTime: "16:00",
+        arrivalTime: "16:45",
+        price: 30,
+        availableSeats: 50
+    },
+    {
+        id: 11,
+        departure: "Rabat",
+        destination: "Fes",
+        departureTime: "17:00",
+        arrivalTime: "19:30",
+        price: 95,
+        availableSeats: 50
+    },
+    {
+        id: 12,
+        departure: "Kenitra",
+        destination: "Fes",
+        departureTime: "17:30",
+        arrivalTime: "20:00",
+        price: 85,
+        availableSeats: 50
+    },
+    {
+        id: 13,
+        departure: "Fes",
+        destination: "Meknes",
+        departureTime: "08:30",
+        arrivalTime: "09:20",
+        price: 35,
+        availableSeats: 50
+    },
+    {
+        id: 14,
+        departure: "Fes",
+        destination: "Oujda",
+        departureTime: "10:00",
+        arrivalTime: "13:30",
+        price: 130,
+        availableSeats: 50
+    },
+    {
+        id: 15,
+        departure: "Meknes",
+        destination: "Rabat",
+        departureTime: "11:00",
+        arrivalTime: "13:30",
+        price: 80,
+        availableSeats: 50
+    },
+    {
+        id: 16,
+        departure: "Meknes",
+        destination: "Casablanca",
+        departureTime: "12:00",
+        arrivalTime: "15:00",
+        price: 105,
+        availableSeats: 50
+    },
+    {
+        id: 17,
+        departure: "Casablanca",
+        destination: "El Jadida",
+        departureTime: "16:30",
+        arrivalTime: "18:00",
+        price: 50,
+        availableSeats: 50
+    },
+    {
+        id: 18,
+        departure: "El Jadida",
+        destination: "Safi",
+        departureTime: "18:30",
+        arrivalTime: "20:30",
+        price: 60,
+        availableSeats: 50
+    },
+    {
+        id: 19,
+        departure: "Marrakech",
+        destination: "Agadir",
+        departureTime: "15:00",
+        arrivalTime: "18:30",
+        price: 100,
+        availableSeats: 50
+    },
+    {
+        id: 20,
+        departure: "Agadir",
+        destination: "Safi",
+        departureTime: "19:00",
+        arrivalTime: "22:00",
+        price: 95,
+        availableSeats: 50
+    }
+];
 
 let choix;
 do {
@@ -203,15 +203,16 @@ do {
   0. Quitter
 `)
     choix = prompt("votre choix : ")
+
     switch (choix) {
         case "1":
             console.log(`=====                         =====
         TRAJETS DISPONIBLES  `)
-                            afficherLestrajets(trips) 
+            afficherLestrajets(trips)
             break;
         case "2":
             console.log("Acheter un ticket ")
-                        console.log(acheterTicket(trips));
+            console.log(acheterTicket(trips));
             break;
         case "3":
             console.log("Afficher les tickets ")
@@ -243,32 +244,47 @@ while (choix != "0");
 
 function afficherLestrajets(trips) {
     for (let trip of trips) {
-        if(trip.availableSeats > 0){
-        console.log(trip)
-    }
-    }
-}
-
-
-function acheterTicket(trips){
-    let nome = +prompt("Nom du passager : ")
-     let Id = +prompt("Identifiant du trajet : ")
-
-    for(let trip of trips){
-        if(trip.id = Id &&  trip.availableSeats > 0){
-            return //creer ticket
-        }
-        else if(trip.availableSeats <= 0) {
-            console.log("trip !!")
+        if (trip.availableSeats > 0){ 
+            console.table(trips)
             break;
         }
-        else{
-            console.log("trajet n'es pas disponible!!")
-        }
-        
     }
 }
+
 const tickets = [];
- function creerTicket(name,Id){
-    
- }
+
+function acheterTicket(trips) {
+    let name = prompt("Nom du passager : ")
+    let Id = +prompt("Identifiant du trajet : ")
+
+    for (let trip of trips) {
+        if (trip.id = Id && trip.availableSeats > 0) {
+            console.log("++Ticket acheté avec succès.")
+            console.log(creerTicket(name, Id, trip.price))
+            return;
+        }
+        else if (trip.availableSeats <= 0) {
+            console.log("Train complet. ")
+            return;
+        }
+        else {
+            console.log("Trajet introuvable.")
+            return;
+        }
+
+    }
+     
+    return tickets
+}
+
+function creerTicket(name, Id, price) {
+
+        let ticket = {
+            id: tickets.length+1 ,
+            passengerName: name ,
+            tripId: Id ,
+            seatNumber: tickets.lenght+1,
+            price: price 
+        }
+       return ticket
+    }
